455f799512fd02031b1e8e10a6b6f836779549b0
SOURAV DAS <isssourav2@gmail.com>
2021-02-10 00:48:23 +0530

    login validation done

--- ERPMEDICAL/wwwroot/Custom-Script/Login.js
+++ ERPMEDICAL/wwwroot/Custom-Script/Login.js
@@ -36,31 +36,57 @@
                     })
             },
             LoginSubmit: function () {
-                var vm = this;
-                debugger;
-                var loginVM = {
-                    UserName: document.getElementById("username").value,
-                    Password: document.getElementById("password").value,
-                    CompanyId: vm.CompanyId
+                if (this.FormValidation() === true) {
+                    var vm = this;
+                    debugger;
+                    var loginVM = {
+                        UserName: document.getElementById("username").value,
+                        Password: document.getElementById("password").value,
+                        CompanyId: vm.CompanyId
+                    }
+                    $.ajax({ url: "/Login", data: loginVM, method: "POST" })
+                        .done(function (response) {
+                            // vm.bugs.splice(0, 0, newBug);
+                            //console.log(data);
+                            debugger;
+                            if (response.status) {
+                                location.replace("/Home/Index");
+                            }
+                            else {
+                                //location.replace("/User/Login");
+                                toastr.error("Given user-name or password is incorrect!", "INVALID INPUT", { positionClass: 'toast-top-center', containerId: 'toast-top-center', "showMethod": "slideDown", "hideMethod": "slideUp", timeOut: 3500 });
+                                return false;
+                            }
+                            //location.replace("/Puchase/PO");
+                            //Redirect to new page
+                        }).fail(function () {
+                            // toastr.error("Can not add new bug!");
+                        }).always(function () {
+                            //vm.clearData();
+                        });
                 }
-                $.ajax({ url: "/Login", data: loginVM, method: "POST" })
-                    .done(function (response) {
-                        // vm.bugs.splice(0, 0, newBug);
-                        //console.log(data);
-                        debugger;
-                        if (response.status) {
-                            location.replace("/Home/Index");
-                        }
-                        else {
-                            location.replace("/User/Login");
-                        }
-                        //location.replace("/Puchase/PO");
-                        //Redirect to new page
-                    }).fail(function () {
-                        // toastr.error("Can not add new bug!");
-                    }).always(function () {
-                        //vm.clearData();
-                    });
+                else {
+                    event.preventDefault();
+                }
+            },
+
+            FormValidation: function () {
+                if (document.getElementById("username").value === "") {                    
+                    toastr.error("User name can't be left blank!", "INVALID INPUT", { positionClass: 'toast-top-center', containerId: 'toast-top-center', "showMethod": "slideDown", "hideMethod": "slideUp", timeOut: 5000 });
+                    return false;
+                }
+
+                if (document.getElementById("password").value === "") {
+                    toastr.error("Password can't be left blank!", "INVALID INPUT", { positionClass: 'toast-top-center', containerId: 'toast-top-center', "showMethod": "slideDown", "hideMethod": "slideUp", timeOut: 5000 });
+                    return false;
+                }
+
+                if (document.getElementById("ddlComapany").value === "0") {
+                    toastr.error("Please select company!", "INVALID INPUT", { positionClass: 'toast-top-center', containerId: 'toast-top-center', "showMethod": "slideDown", "hideMethod": "slideUp", timeOut: 5000 });
+                    return false;
+                }
+
+                return true;
             }
         }
     })
